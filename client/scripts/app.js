96a261503915b26e9cf3dfa7e5c0d4e38130b3f7
Hack Reactor Students <walkermellema@gmail.com>
2014-06-16 14:57:53 -0700

    Added send and fetch functionality

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,48 @@
 // YOUR CODE HERE:
+//
+//var url = 'https://api.parse.com/1/classes/chatterbox';
+
+var app = {};
+
+app.init = function(){
+  this.server = 'https://api.parse.com/1/classes/chatterbox';
+};
+
+app.send = function(message){
+  $.ajax({
+    // always use this url
+    url: this.server,
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+      console.log(data);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message');
+      console.log(data);
+    }
+  });
+};
+
+app.fetch = function(){
+  $.ajax({
+    // always use this url
+    url: this.server,
+    type: 'GET',
+    //data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Get succeeded');
+      console.log(data);
+    },
+    error: function (data) {
+      // see: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to get message(s)');
+      console.log(data);
+    }
+  });
+};
 
